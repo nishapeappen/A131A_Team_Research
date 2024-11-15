8fe1a2134a9d6ebd9a3fe376796775db9b474200
Nisha Eappen <np23abz@herts.ac.uk>
2024-11-15 21:21:33 +0000

    Hypothesis testing using t.test()

---
+++ hypothesis_testing(t_tetsing).R
@@ -0,0 +1,7 @@
+library(dplyr)
+data <-read.csv("C:/Users/DELL/Documents/A131A_Team_Research/Golf.csv")
+head(data)
+summary(data$Current)
+summary(data$New)
+t_test_result <- t.test(data$Current, data$New, paired = FALSE)
+print(t_test_result)
