db456bba71a4a92fec6978cb773970432931886c
Nisha Eappen <np23abz@herts.ac.uk>
2024-11-23 22:21:55 +0000

    Contains all finding and solutions

---
+++ Findings & Results.R
@@ -0,0 +1,12 @@
+Independent varible - type of golf(i.e., with coating & without coating)
+Dependent variable - Driving Distance of the golf ball
+
+
+RQ - Is there difference in the mean of driving distance of the golf ball between with coating and without coating of the golf ball?
+  
+Stastical Test:
+  To find whether it is null hypothesis or alternative hypothesis, we use t test.
+  From the t test, we got a p value (0.188), which is greater than the 0.05.
+  From the solution, we can't reject null hypothesis, so this is null hypothesis.
+
+From all these findings, we can chart our visual representation using histogram & box plot.
