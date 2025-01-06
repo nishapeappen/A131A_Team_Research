cb9e585f8516f49067491e43ba713ea1d8a2ae9c
alvinjoshy <aj23ads@herts.ac.uk>
2025-01-06 20:10:39 +0000

    Update Boxplot.R

--- Boxplot.R
+++ Boxplot.R
@@ -1,3 +1,9 @@
+library(dplyr)
+data <-read.csv("Golf.csv")
+head(data)
+summary(data$Current)
+summary(data$New)
+
 boxplot(data$Current, data$New,names = c ("without coating","with coating"),
         main ="Driving distance of golf ball",
         ylab ="DISTANCE (yards)")
